78f146131ac646513117e08ec7a33f94ffc3926b
ahtisham101 <ahtisham_101@outlook.com>
2023-10-21 11:20:01 +0500

    feat: add carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-
+import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
 
@@ -34,7 +34,7 @@ const Home = async () => {
             <Searchbar />
           </div>
 
-         
+          <HeroCarousel />
         </div>
       </section>
 
